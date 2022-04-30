1af74b07d991e4e53235125babd66065a2fb7585
immmdreza <ir310022@gmail.com>
2022-04-30 23:03:29 +0430

    lets try

---
+++ src/telegrambots/custom/client.py
@@ -0,0 +1,69 @@
+from typing import Optional
+from telegrambots.wrapper.client import TelegramBotsClient
+from telegrambots.wrapper.types.methods import SendMessage
+from telegrambots.wrapper.types.objects import (
+    MessageEntity,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    ForceReply,
+)
+
+
+class CustomTelegramBotsClient(TelegramBotsClient):
+    def __init__(self, token: str):
+        super().__init__(token)
+
+    async def send_message(
+        self,
+        chat_id: int,
+        text: str,
+        parse_mode: Optional[str] = None,
+        entities: Optional[list[MessageEntity]] = None,
+        disable_web_page_preview: Optional[bool] = None,
+        disable_notification: Optional[bool] = None,
+        protect_content: Optional[bool] = None,
+        reply_to_message_id: Optional[int] = None,
+        allow_sending_without_reply: Optional[bool] = None,
+        reply_markup: Optional[
+            InlineKeyboardMarkup
+            | ReplyKeyboardMarkup
+            | ReplyKeyboardRemove
+            | ForceReply
+        ] = None,
+    ):
+        """Send text message to chat.
+
+        Args:
+            chat_id (`int`): Unique identifier for the target chat or username of the target channel
+            text (`str`): Text of the message to be sent
+            parse_mode (`Optional[str]`, optional): Send Markdown or HTML, if you want Telegram apps to show bold,
+                italic, fixed-width text or inline URLs in your bot's message.
+            entities (`Optional[list[MessageEntity]`], optional): List of special entities that appear in message text,
+                which can be specified instead of parse_mode.
+            disable_web_page_preview (`Optional[bool]`, optional): Disables link previews for links in this message.
+            disable_notification (`Optional[bool]`, optional): Sends the message silently.
+            protect_content (`Optional[bool]`, optional): If True, the message content will be protected.
+            reply_to_message_id (`Optional[int]`, optional): reply to message id.
+            allow_sending_without_reply (`Optional[bool]`, optional): If True, the message will be sent anyway.
+            reply_markup (`Optional[ InlineKeyboardMarkup  |  ReplyKeyboardMarkup  |  ReplyKeyboardRemove  |  ForceReply ]`, optional):
+                Additional interface options. An object for an inline keyboard, custom reply keyboard, ...
+
+        Returns:
+            `Message`: On success, the sent message is returned.
+        """
+
+        return await self(
+            SendMessage(
+                chat_id=chat_id,
+                text=text,
+                parse_mode=parse_mode,
+                entities=entities,
+                disable_web_page_preview=disable_web_page_preview,
+                disable_notification=disable_notification,
+                protect_content=protect_content,
+                reply_to_message_id=reply_to_message_id,
+                allow_sending_without_reply=allow_sending_without_reply,
+                reply_markup=reply_markup,
+            )
+        )
